521289ec4ead7e8063e2bea24b958a71a8c72551
R3ZAR <rrahimi25@gmail.com>
2023-06-03 19:18:51 +0330

    Abstract Factory

--- src/main/java/org/designpattern/creational/abstractfactory/MainClass.java
+++ src/main/java/org/designpattern/creational/abstractfactory/MainClass.java
@@ -1,7 +1,6 @@
 package org.designpattern.creational.abstractfactory;
 
 import org.designpattern.creational.abstractfactory.app.BuildCar;
-import org.designpattern.creational.abstractfactory.app.CarFactory;
 import org.designpattern.creational.abstractfactory.benz.BenzFactory;
 import org.designpattern.creational.abstractfactory.bmw.BmwFactory;
 
@@ -10,6 +9,7 @@ public class MainClass {
 
         BuildCar buildCar = new BuildCar();
         buildCar.build(new BmwFactory());
+        buildCar.build(new BenzFactory());
 
     }
 }
